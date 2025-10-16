1a4e3aeb31242e4260721c373ac947a4569c899e
alan-r03 <alan68776@gmail.com>
2025-10-16 15:19:58 -0500

    new test file every 4 hours

--- backend/test_db_write.py
+++ backend/test_db_write.py
@@ -1,15 +1,18 @@
 # backend/tests/test_db_write.py
-from app.db.database import SessionLocal
-from app.db import models
+#from app.db.database import SessionLocal
+#from app.db import models
+import time
 
-db = SessionLocal()
-article = models.Article(
-    title="Test Article",
-    url="https://example.com",
-    content="This is just a test entry.",
-    sector="Test",
-)
-db.add(article)
-db.commit()
-db.close()
-print("✅ Inserted test article")
+print(f'test executed successfully@\ntime: {time.time()}')
+
+#db = SessionLocal()
+#article = models.Article(
+#    title=f"Test Article {str(time.time())[:10]}",
+#    url="https://example.com",
+#    content="This is just a test entry.",
+#    sector="Test",
+#)
+#db.add(article)
+#db.commit()
+#db.close()
+#print("✅ Inserted test article")
